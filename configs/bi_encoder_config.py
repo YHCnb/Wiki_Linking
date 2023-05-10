ecb4069bcbe3d63ce6d0cbe9655bcaa632310d61
YHCnb <112797916+YHCnb@users.noreply.github.com>
2023-05-10 22:43:47 +0800

    Update bi_encoder_config.py

--- configs/bi_encoder_config.py
+++ configs/bi_encoder_config.py
@@ -2,7 +2,7 @@ class Bi_encoder_config:
     def __init__(self):
         self.config = {
             'bert_path': "bert-base-chinese",  # bert-base地址
-            'out_dim': 10000,  # CLS线性头输出的维度
+            'out_dim': 2000,  # CLS线性头输出的维度
             "ENT_START_TAG": "[unused0]",  # 3个特殊token的标记
             "ENT_END_TAG": "[unused1]",
             "ENT_TITLE_TAG": "[unused2]",
